65e37919d9cace305a731062acd1c3585ad45756
ahmed <af6394158@gmil.com>
2023-03-14 12:22:42 +0200

    working on navbar

--- Js/mainScript.js
+++ Js/mainScript.js
@@ -1,3 +1,5 @@
+let games
+	,gamesTitle=[];
 const options = {
 	method: 'GET',
 	headers: {
@@ -6,18 +8,50 @@ const options = {
 	}
 };
 
-async function getData(){
-	const data=await fetch('https://free-to-play-games-database.p.rapidapi.com/api/filter?tag=3d.mmorpg.fantasy.pvp&platform=pc', options)
-	const result=await data.json();
-	const games=Array.from(result)
-	for(let i=0 ;i<games.length;i++){
-		console.log(games[i].id);
-	}
 
-	/* console.log(games.length)
-	console.log(result[3].id) */
+async function getData(){
+
+const data=await fetch('https://free-to-play-games-database.p.rapidapi.com/api/games', options);
+	const result=await data.json();
+	games=Array.from(result)
+		console.log(games)
+	for(let i=0 ;i<games.length;i++){
+		gamesTitle.push(games[i].title);
+		
+	}	
 }
- getData();
-	/* .then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err)); */
+
+getData();
+
+
+$('#searchIcon').click(function(){
+	$('#searchInp').toggle('slow')
+	$('#searchInp').keydown(function(e){
+		if(e.key=='Enter'){
+			let searchedGame = games.find(o => {
+				if(o.title === $('#searchInp').val()){
+					return o.title === $('#searchInp').val();
+					
+				}
+				
+			});
+			if(searchedGame===undefined){
+				console.log('Game not found');
+			}
+			else{
+				console.log(searchedGame);
+			}
+	
+
+			$('#searchInp').val('');
+		}
+		
+		
+	})
+})
+
+$('.navbar-toggler').click(function(){
+	$('.bar_1').toggleClass('closeBar1')
+	$('.bar_2').toggleClass('closeBar2')
+	$('.bar_3').toggleClass('closeBar3')
+})
